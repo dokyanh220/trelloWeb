5be79e2c7bf0883da9864a78c7a576ce993158ec
dokyanh220 <kyanhdubo2107@gmail.com>
2025-08-27 21:48:56 +0700

    thời gian trôi nhanh quá, anh muốn gặp em

--- src/pages/Boards/BoardContent/ListColumns/ListColumns.jsx
+++ src/pages/Boards/BoardContent/ListColumns/ListColumns.jsx
@@ -1,5 +1,7 @@
 import Box from '@mui/material/Box'
 import Column from './Column/Column'
+import Button from '@mui/material/Button'
+import NoteAddIcon from '@mui/icons-material/NoteAdd'
 
 function ListColumns() {
   return (
@@ -16,6 +18,28 @@ function ListColumns() {
       }}
     >
       <Column />
+      <Box
+        sx={{
+          width: 'fit-content',
+          height: 'fit-content',
+          mx: 2,
+          borderRadius: '6px',
+          bgcolor: '#ffffff3d'
+        }}
+      >
+        <Button
+          startIcon={<NoteAddIcon />}
+          sx={{
+            color: 'white',
+            width: '100%',
+            alignItems: 'center',
+            pl: 2.5,
+            py: 1
+          }}
+        >
+          Add new column
+        </Button>
+      </Box>
     </Box>
   )
 }
