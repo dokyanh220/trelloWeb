9b5b1ff7979e767ed2f89e2c65a46c74b0ea615a
dokyanh220 <kyanhdubo2107@gmail.com>
2025-08-29 01:35:46 +0700

    drag-move DndKit

--- src/pages/Boards/BoardContent/BoardContent.jsx
+++ src/pages/Boards/BoardContent/BoardContent.jsx
@@ -1,22 +1,67 @@
+import { useEffect, useState } from 'react'
 import Box from '@mui/material/Box'
 import ListColumns from './ListColumns/ListColumns'
 import { mapOrder } from '~/utils/sorts'
+import {
+  DndContext,
+  PointerSensor,
+  MouseSensor,
+  TouchSensor,
+  useSensor,
+  useSensors
+} from '@dnd-kit/core'
+import { arrayMove } from '@dnd-kit/sortable'
 
 function BoardContent({ board }) {
-  const orderedColumns = mapOrder(board?.columns, board?.columnOrderIds, '_id')
+  const pointerSensor = useSensor(PointerSensor, {
+    activationConstraint: { distance: 10 }
+  })
+  const mouseSensor = useSensor(MouseSensor, {
+    activationConstraint: { distance: 10 }
+  })
+  const touchSensor = useSensor(TouchSensor, {
+    activationConstraint: { delay: 250, tolerance: 500 }
+  })
+  const sensors = useSensors(pointerSensor, mouseSensor, touchSensor)
+  const [orderedColumns, setOrderedColumns] = useState([])
+
+  useEffect(() => {
+    setOrderedColumns(mapOrder(board?.columns, board?.columnOrderIds, '_id'))
+  }, [board])
+
+  const handleDragEnd = (e) => {
+    const { active, over } = e
+
+    if (!over) return
+    if (active.id !== over.id) {
+      const oldIndex = orderedColumns.findIndex(
+        (column) => column._id === active.id
+      )
+      const newIndex = orderedColumns.findIndex(
+        (column) => column._id === over.id
+      )
+      const dndOrderedColumns = arrayMove(orderedColumns, oldIndex, newIndex)
+      // const dndOrderedColumnsIds = dndOrderedColumns.map((column) => column.id)
+      // console.log(dndOrderedColumnsIds)
+
+      setOrderedColumns(dndOrderedColumns)
+    }
+  }
   return (
-    <Box
-      sx={{
-        bgcolor: (theme) =>
-          theme.palette.mode === 'dark' ? '#34495e' : '#19764d2',
-        width: '100%',
-        height: (theme) =>
-          `calc(100vh - ${theme.trello.boardBarHeight} - ${theme.trello.appBarHeight})`,
-        p: '10px 0'
-      }}
-    >
-      <ListColumns columns={orderedColumns} />
-    </Box>
+    <DndContext onDragEnd={handleDragEnd} sensors={sensors}>
+      <Box
+        sx={{
+          bgcolor: (theme) =>
+            theme.palette.mode === 'dark' ? '#34495e' : '#19764d2',
+          width: '100%',
+          height: (theme) =>
+            `calc(100vh - ${theme.trello.boardBarHeight} - ${theme.trello.appBarHeight})`,
+          p: '10px 0'
+        }}
+      >
+        <ListColumns columns={orderedColumns} />
+      </Box>
+    </DndContext>
   )
 }
 
