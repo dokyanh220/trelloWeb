96142833d27065daaf5976b976c614a7e03cbcd5
dokyanh220 <kyanhdubo2107@gmail.com>
2025-09-01 00:33:41 +0700

    conneconfig

--- trello-api/src/server.js
+++ trello-api/src/server.js
@@ -1,16 +1,33 @@
-const express = require('express')
-const { mapOrder } = require('~/utils/sorts.js')
+/* eslint-disable no-console */
+/* eslint-disable no-unused-vars */
+import express from 'express'
+import exitHook from 'async-exit-hook'
+import { CONNECT_DB, CLOSE_DB } from '~/config/mongodb'
+import { env } from './config/environment'
 
-const app = express()
+const START_SERVER = () => {
+  const app = express()
 
-const hostname = 'localhost'
-const port = 8017
+  app.get('/', (req, res) => {
+    res.end('<h1>Hello World!</h1><hr>')
+  })
 
-app.get('/', (req, res) => {
-  res.end('<h1>Hello World!</h1><hr>')
-})
+  app.listen(env.APP_PORT, env.APP_HOST, () => {
+    console.log(`Back-end Server running successfully at http://${env.APP_HOST}:${env.APP_PORT}/`)
+  })
 
-app.listen(port, hostname, () => {
-  // eslint-disable-next-line no-console
-  console.log(`Server running at http://${ hostname }:${ port }/`)
-})
+  // Cleanup trước khi dừng server
+  exitHook(() => {
+    console.log('Disconnected from MongoDB Cloud Atlas!')
+    CLOSE_DB()
+  })
+}
+
+console.log('Connecting to MongoDB Cloud Atlas...')
+CONNECT_DB()
+  .then(() => { console.log('Connected to MongoDB Cloud Atlas!') })
+  .then(() => START_SERVER())
+  .catch(error => {
+    console.error(error)
+    process.exit(0)
+  })
