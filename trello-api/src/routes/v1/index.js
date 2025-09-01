4a8615b7ef1cf4c885744ab7d613c1b362cf351d
dokyanh220 <kyanhdubo2107@gmail.com>
2025-09-01 14:59:14 +0700

    router get & post

---
+++ trello-api/src/routes/v1/index.js
@@ -0,0 +1,13 @@
+import express from 'express'
+import { StatusCodes } from 'http-status-codes'
+import { boardRoutes } from './boardRoutes'
+
+const Router = express.Router()
+
+Router.get('/status', (req, res) => {
+  res.status(StatusCodes.OK).json({ message: 'Api v1 are ready' })
+})
+
+Router.use('/boards', boardRoutes)
+
+export const APIs_V1 = Router
