93e3ce857c81b32ccd53fac8f4f1ba910171b6fe
dokyanh220 <kyanhdubo2107@gmail.com>
2025-09-01 19:41:28 +0700

    validate data

---
+++ trello-api/src/validations/boardValidation.js
@@ -0,0 +1,38 @@
+import Joi from 'joi'
+import { StatusCodes } from 'http-status-codes'
+
+const createNew = async (req, res, next) => {
+  const correctCondition = Joi.object({
+    title: Joi.string().required().min(3).max(50).trim().strict().messages({
+      'any.required': 'Title is required',
+      'string.empty': 'Title is not allowed to be empty',
+      'string.min': 'Title min 3 chars',
+      'string.max': 'Title max 50 chars',
+      'string.trim': 'Title must not have leading or trailing whitespace'
+    }),
+    description: Joi.string().required().min(3).max(255).trim().strict().messages({
+      'any.required': 'Title is required',
+      'string.empty': 'Title is not allowed to be empty',
+      'string.min': 'Title min 3 chars',
+      'string.max': 'Title max 255 chars',
+      'string.trim': 'Title must not have leading or trailing whitespace'
+    })
+  })
+
+  try {
+    console.log(req.body)
+
+    await correctCondition.validateAsync(req.body, { abortEarly: false })
+
+    res.status(StatusCodes.CREATED).json({ message: 'Post from validation: create new board' })
+  } catch (error) {
+    console.log(error)
+    res.status(StatusCodes.UNPROCESSABLE_ENTITY).json({
+      errors: new Error(error).message
+    })
+  }
+}
+
+export const boardValidation = {
+  createNew
+}
