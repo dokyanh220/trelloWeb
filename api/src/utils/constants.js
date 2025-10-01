b1f2b1bfe274f957e2fdcd639f92bfbe15ed81d6
dokyanh220 <kyanhdubo2107@gmail.com>
2025-10-01 15:21:04 +0700

    render

--- api/src/utils/constants.js
+++ api/src/utils/constants.js
@@ -1,10 +1,11 @@
 import { env } from '~/config/environment'
 
 export const WHITELIST_DOMAINS = [
-  'http://localhost:5173',
+  // 'http://localhost:5173',
   'https://trello-dokyanh.vercel.app/'
   //...
 ]
+
 
 export const BOARD_TYPES = {
   PUBLIC: 'public',
