44d1f80690389e063168a7484f852ce9fe9360ca
dokyanh220 <kyanhdubo2107@gmail.com>
2025-10-01 15:14:47 +0700

    render

--- web/src/utils/constanst.js
+++ web/src/utils/constanst.js
@@ -3,6 +3,6 @@ if (process.env.BUILD_MODE === 'dev') {
   apiRoot = 'http://localhost:8017'
 }
 if (process.env.BUILD_MODE === 'prod') {
-  apiRoot = ''
+  apiRoot = 'https://trelloweb-hyk3.onrender.com'
 }
 export const API_ROOT = apiRoot
