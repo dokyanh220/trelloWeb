c10677fc199a0e8c637b47442443dcc6a1dd777f
dokyanh220 <kyanhdubo2107@gmail.com>
2025-10-01 16:44:59 +0700

    vercel

--- api/src/utils/constants.js
+++ api/src/utils/constants.js
@@ -2,7 +2,7 @@ import { env } from '~/config/environment'
 
 export const WHITELIST_DOMAINS = [
   // 'http://localhost:5173',
-  'https://trello-dokyanh.vercel.app/'
+  'https://trello-dokyanh.vercel.app'
   //...
 ]
 
